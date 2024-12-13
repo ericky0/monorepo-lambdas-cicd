0cb8b66dda4334aa1a39be4529b236aee3779e2c
erickhogarth <xandesteamgames977@gmail.com>
2024-12-13 17:23:41 -0300

    logs

--- src/lambdas/lambda-axios/index.ts
+++ src/lambdas/lambda-axios/index.ts
@@ -4,7 +4,7 @@ import { generateLog } from './service'
 export const handler = async () => {
   // test to see if only axios dependency will be published to lambda
   axios.get('http://example')
-  generateLog('test2')
+  generateLog('test3')
 
   return {
     statusCode: 200,
