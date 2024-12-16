6718f824fb1da80add26aa1ab94a6ee3b1b0580d
erickhogarth <xandesteamgames977@gmail.com>
2024-12-16 18:10:55 -0300

    commit 1

--- src/lambdas/lambda-axios/index.ts
+++ src/lambdas/lambda-axios/index.ts
@@ -4,6 +4,7 @@ import { generateLog } from './service'
 export const handler = async () => {
   // test to see if only axios dependency will be published to lambda
   axios.get('http://example')
+  console.log('commit 1')
   generateLog('test1112334')
 
   return {
